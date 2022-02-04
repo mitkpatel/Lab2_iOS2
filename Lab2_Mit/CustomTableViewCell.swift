71a5ec5d46dc2f3af9df43741dfbf6ecfb0a8b40
mitkpatel <mitkpatel09@gmail.com>
2022-02-04 16:41:20 -0500

    first

---
+++ Lab2_Mit/CustomTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  CustomTableViewCell.swift
+//  Lab2_Mit
+//
+//  Created by user214044 on 31/1/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    lazy var backView: UIView = {
+        let view = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width + 50, height: 110))
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    
+    lazy var userImage: UIImageView = {
+        let userImage = UIImageView(frame: CGRect(x: 4, y: 6, width: 108, height: 108))
+        userImage.contentMode = .scaleAspectFit
+        return userImage
+    }()
+    
+    lazy var namelbl: UILabel = {
+        let lbl = UILabel(frame: CGRect(x: 116, y: 8, width: backView.frame.width - 116, height: 30))
+        lbl.textAlignment = .left
+        lbl.font = UIFont.boldSystemFont(ofSize: 18)
+        return lbl
+    }()
+    
+    lazy var agelbl: UILabel = {
+        let lbl = UILabel(frame: CGRect(x: 116, y: 42, width: backView.frame.width - 116, height: 30))
+        lbl.textAlignment = .left
+        return lbl
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func layoutSubviews() {
+        contentView.backgroundColor = UIColor.clear
+        backgroundColor = UIColor.clear
+        backView.layer.cornerRadius = 5
+        backView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        addSubview(backView)
+        backView.addSubview(userImage)
+        backView.addSubview(namelbl)
+        backView.addSubview(agelbl)
+    }
+
+}
---
+++ Lab2_Mit/ViewController.swift
@@ -0,0 +1,160 @@
+//
+//  ViewController.swift
+//  Lab2_Mit
+//
+//  Created by user214044 on 1/29/22.
+//
+
+import UIKit
+
+class UserModel {
+    var userImage: UIImage?
+    var name: String?
+    var age: String?
+    
+    init(user: UIImage, name: String, age: String) {
+        self.userImage = user
+        self.name = name
+        self.age = age
+    }
+}
+
+class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 52))
+        button.setTitle("Log in", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.center = view.center
+    }
+
+    @objc func didTapButton() {
+        // Create and present tab bar comtroller
+        let tabBarVC = UITabBarController()
+        let HomeVC = UINavigationController(rootViewController: HomeVC())
+        let FavoriteVC =  UINavigationController(rootViewController: FavoriteVC())
+        let AboutVC = UINavigationController(rootViewController: AboutVC())
+        
+        HomeVC.title = "Home"
+        AboutVC.title = "About"
+        FavoriteVC.title = "List"
+        
+        UITabBar.appearance().barTintColor = UIColor.gray
+        tabBarVC.tabBar.isTranslucent = false
+        tabBarVC.tabBar.tintColor = .blue
+        tabBarVC.setViewControllers([ HomeVC , FavoriteVC , AboutVC ], animated: false)
+        
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true)
+        
+        guard let items = tabBarVC.tabBar.items else {
+            return
+        }
+        
+        let images = ["house", "star", "gear"]
+        
+        for x in 0..<items.count {
+            items[x].badgeValue = "1"
+            items[x].image = UIImage(systemName: images[x])
+        }
+    
+    }
+}
+
+
+class HomeVC: UIViewController {
+    
+    var tableView = UITableView()
+    
+    var userModel = [UserModel]()
+    
+    var userArr = [UserModel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Home"
+
+        userArr.append(UserModel(user: UIImage.init(named: "Charlize Theron")!, name: "Mit", age: "24"))
+        userArr.append(UserModel(user: UIImage.init(named: "Charlize Theron")!, name: "Amber Heard", age: "32"))
+        userArr.append(UserModel(user: UIImage.init(named: "Emma Stone")!, name: "Emma Stone", age: "30"))
+        userArr.append(UserModel(user: UIImage.init(named: "Amber Heard")!, name: "Angelina Jolie", age: "43"))
+        userArr.append(UserModel(user: UIImage.init(named: "Jennifer Lawrence")!, name: "Scarlett Johansson", age: "34"))
+        userArr.append(UserModel(user: UIImage.init(named: "Angelina Jolie")!, name: "Jennifer Lawrence", age: "28"))
+        userArr.append(UserModel(user: UIImage.init(named: "Scarlett Johansson")!, name: "Charlize Theron", age: "43"))
+
+        
+        setTableView()
+    }
+    
+    
+    func setTableView() {
+        tableView.frame = self.view.frame
+        tableView.backgroundColor = UIColor.clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorColor = UIColor.clear
+        tableView.backgroundColor = UIColor.red
+        self.view.addSubview(tableView)
+        
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userArr.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//        cell.textLabel?.text = "\(indexPath.row)"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as?
+            CustomTableViewCell else { fatalError("Unable to create cell")}
+        cell.userImage.image = userArr[indexPath.row].userImage
+        cell.namelbl.text = userArr[indexPath.row].name
+        cell.agelbl.text = userArr[indexPath.row].age
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 118
+    }
+    
+}
+
+class FavoriteVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        title = "Favorite"
+    }
+}
+
+class AboutVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        title = "About"
+    }
+}
+
+
